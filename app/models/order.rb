511e7cbd986a5588c701375d432e490cdb55f84b
Sia <siakaramalegos@gmail.com>
2015-11-24 14:21:31 -0600

    Linked order to cart line items and clear cart when order saved:

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :line_items, dependent: :destroy
+
+  PAYMENT_TYPES = ['Credit Card', 'Paypal', 'Monopoly Money']
+
+  validates :ship_name, :ship_address, :payment_type, presence: true
+  validates :payment_type, inclusion: PAYMENT_TYPES
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 end
